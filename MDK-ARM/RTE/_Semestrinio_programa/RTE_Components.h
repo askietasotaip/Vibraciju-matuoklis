a5bb6e9a1b9ca81bc7a286eec8b4f0f453bd7149
Lukas Alekna <lukasalekna0@gmail.com>
2024-04-30 14:48:42 +0300

    pirmas commit - cube sablonas ir keil projektas

---
+++ MDK-ARM/RTE/_Semestrinio_programa/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Semestrinio programa' 
+ * Target:  'Semestrinio programa' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32l0xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
