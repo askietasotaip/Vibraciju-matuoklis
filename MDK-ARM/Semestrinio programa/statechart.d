62f9695c4af6348117b0d21ad7b572283f8d34a1
Lukas Alekna <lukasalekna0@gmail.com>
2024-05-02 15:19:56 +0300

    prideta itemis diagrama, nustatytas timeris

---
+++ MDK-ARM/Semestrinio programa/statechart.d
@@ -0,0 +1,7 @@
+semestrinio\ programa/statechart.o: ..\Itemio\src-gen\Statechart.c \
+  ..\Itemio\src-gen\..\src\sc_types.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdbool.h \
+  ..\Itemio\src-gen\Statechart.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  ..\Itemio\src-gen\Statechart_required.h
